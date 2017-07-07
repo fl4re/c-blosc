79db5ddf787d7916c450203fd1897036c5e0603c
Mark Barnes <mark.barnes@starbreeze.com>
2017-07-07 16:19:21 -0700

    Revert "Added build script and consolidate artifacts"

--- CLEAN_BUILD.bat
+++
@@ -1,8 +0,0 @@
-setlocal EnableDelayedExpansion
-
-git reset --hard || exit /b 1
-git clean -ffdx || exit /b 1
-
-::No submodules in this repo, no need to use these commands
-::git submodule foreach --recursive git reset --hard || exit /b 1
-::git submodule foreach --recursive git clean -ffdx || exit /b 1
