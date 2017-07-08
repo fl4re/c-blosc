e2808d09f491fb751b3656a17178459f95361fd1
Krunal Patel <krunal.patel@starbreeze.com>
2017-07-07 17:51:31 -0700

    using local installed prades at specific version

--- publish_binary_on_windows.bat
+++ publish_binary_on_windows.bat
@@ -3,10 +3,13 @@ setlocal EnableDelayedExpansion
 git rev-parse --is-inside-work-tree
 if errorlevel 1 exit /b %errorlevel% 
 
+call npm install
+if errorlevel 1 exit /b %errorlevel%
+
 call npm --no-git-tag-version version patch
 if errorlevel 1 exit /b %errorlevel%
 
-call prades publish -v
+call node_modules\.bin\prades publish -v
 if errorlevel 1 exit /b %errorlevel% 
 
 exit /b 0
